f9573e54a626e837b30b83d405b4736bcc806e48
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-04-21 13:12:57 +0200

    add condition to open deals

--- client/src/bundles/open-deals/pages/open-deals/open-deals.tsx
+++ client/src/bundles/open-deals/pages/open-deals/open-deals.tsx
@@ -6,12 +6,14 @@ import { useAppDispatch, useAppSelector } from '~/bundles/common/hooks/hooks';
 import { OpenDealsItem } from '../../components/components.js';
 import { actions as openDealsActions } from '../../store/index.js';
 import styles from './styles.module.scss';
+import { DataStatus } from '~/bundles/common/enums/data-status.js';
 
 const OpenDeals = () => {
     const dispatch = useAppDispatch();
 
-    const { openDeals } = useAppSelector(state => ({
-        openDeals: state.openDeals.openDeals
+    const { openDeals, dataStatus } = useAppSelector(state => ({
+        openDeals: state.openDeals.openDeals,
+        dataStatus: state.openDeals.dataStatus
       }));
     
       const handleLoadOpenDeals = useCallback(
@@ -27,9 +29,12 @@ const OpenDeals = () => {
         <div className={styles.open_deals_page}>
             <h2 className={styles.open_deals_header}>Open Deals</h2>
             <div className={styles.open_deals_items}>
-                {openDeals?.map((item, index) => (
-                    <OpenDealsItem key={index} item={item} image={openDealsImages[index]}/>
-                ))}
+                {openDeals && dataStatus === DataStatus.FULFILLED 
+                    ? ( openDeals?.map((item, index) => (
+                        <OpenDealsItem key={index} item={item} image={openDealsImages[index]}/>
+                      )))
+                    : <div>There are not open deals</div>
+            }
             </div>
         </div>
     )
